94fda2ebf9860eecbb5ec5547232176516d7e897
BRUNOMOREAUGARCIASANCHEZ <48569806+BRUNOMOREAUGARCIASANCHEZ@users.noreply.github.com>
2024-10-18 14:02:04 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/proyectofinal/CrearActivity.kt
@@ -0,0 +1,13 @@
+package com.example.proyectofinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear)
+    }
+}
---
+++ app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.proyectofinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    int x;
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
